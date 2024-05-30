bf14b23d0048b080cbaec8f021afb605bea4638e
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-05-30 01:33:55 -0700

    cmp window borders

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -44,9 +44,9 @@ vim.keymap.set('v', '<A-k>', ":m '<-2<cr>gv=gv", { desc = 'Move Up' })
 vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join line' })
 
 -- Keep cursor centred while scrolling up and down
--- vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll up' })
--- vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll down' })
-
+vim.keymap.set('n', '<C-d>', '12<C-d>zz', { desc = 'Scroll up' })
+vim.keymap.set('n', '<C-u>', '12<C-u>zz', { desc = 'Scroll down' })
+--
 -- Delete previous word in insert mode
 vim.keymap.set('i', '<A-BS>', '<Esc>cvb', { desc = 'Delete previous word in insert mode' })
 
--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -22,5 +22,18 @@ return {
     dependencies = {
       'MunifTanjim/nui.nvim',
     },
+    routes = {
+      {
+        filter = {
+          event = 'msg_show',
+          any = {
+            { find = '%d+L, %d+B' },
+            { find = '; after #%d+' },
+            { find = '; before #%d+' },
+          },
+        },
+        view = 'mini',
+      },
+    },
   },
 }
