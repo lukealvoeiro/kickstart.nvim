4d2654c0e1a6358e0efc069353c851f2852461c4
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-06-02 11:23:34 -0700

    yaas

--- init.lua
+++ init.lua
@@ -17,17 +17,6 @@ require 'config.autocmds'
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
--- Highlight when yanking (copying) text
---  Try it with `yap` in normal mode
---  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
@@ -363,11 +352,22 @@ require('lazy').setup({
             })
           end
           -- The following autocommand is used to enable inlay hints in your
-          -- code, if the language server you are using supports them
-          --
-          vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
+          -- code, if the language server you are using supports them:w
 
-          vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+          local borderTable = {
+            { '╭', 'BorderBG' },
+            { '─', 'BorderBG' },
+            { '╮', 'BorderBG' },
+            { '│', 'BorderBG' },
+            { '╯', 'BorderBG' },
+            { '─', 'BorderBG' },
+            { '╰', 'BorderBG' },
+            { '│', 'BorderBG' },
+          }
+          --
+          vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = borderTable })
+
+          vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = borderTable })
 
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
@@ -475,46 +475,6 @@ require('lazy').setup({
     opts = { signs = false },
   },
 
-  { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require('mini.ai').setup { n_lines = 500 }
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
-
-      -- Highlight the word under the cursor
-      require('mini.cursorword').setup()
-
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
-    end,
-  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -1,39 +1,72 @@
-return {
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    opts = {
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
-        },
-      },
-    },
-    -- you can enable a preset for easier configuration
-    presets = {
-      bottom_search = true, -- use a classic bottom cmdline for search
-      command_palette = true, -- position the cmdline and popupmenu together
-      long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = false, -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = false, -- add a border to hover docs and signature help
-    },
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-    },
-    routes = {
-      {
-        filter = {
-          event = 'msg_show',
-          any = {
-            { find = '%d+L, %d+B' },
-            { find = '; after #%d+' },
-            { find = '; before #%d+' },
-          },
-        },
-        view = 'mini',
-      },
-    },
-  },
-}
+return {}
+-- return {
+--   {
+--     'folke/noice.nvim',
+--     event = 'VeryLazy',
+--     opts = {
+--       lsp = {
+--         override = {
+--           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+--           ['vim.lsp.util.stylize_markdown'] = true,
+--           ['cmp.entry.get_documentation'] = true,
+--         },
+--       },
+--       popupmenu = {
+--         enabled = true,
+--         backend = 'nui',
+--       },
+--     },
+--     -- you can enable a preset for easier configuration
+--     presets = {
+--       command_palette = true, -- position the cmdline and popupmenu together
+--       long_message_to_split = true, -- long messages will be sent to a split
+--       inc_rename = true, -- enables an input dialog for inc-rename.nvim
+--     },
+--     cmdline_popup = {
+--       position = {
+--         row = 5,
+--         col = '50%',
+--       },
+--       size = {
+--         width = 60,
+--         height = 'auto',
+--       },
+--     },
+--     popupmenu = {
+--       enabled = true, -- enables the Noice popupmenu UI
+--       backend = 'cmp', -- backend to use to show regular cmdline completions
+--       relative = 'editor',
+--       position = {
+--         row = 8,
+--         col = '50%',
+--       },
+--       size = {
+--         width = 60,
+--         height = 10,
+--       },
+--       border = {
+--         style = 'rounded',
+--         padding = { 0, 1 },
+--       },
+--       win_options = {
+--         winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+--       },
+--     },
+--     dependencies = {
+--       'MunifTanjim/nui.nvim',
+--     },
+--     routes = {
+--       {
+--         filter = {
+--           event = 'msg_show',
+--           any = {
+--             { find = '%d+L, %d+B' },
+--             { find = '; after #%d+' },
+--             { find = '; before #%d+' },
+--           },
+--         },
+--         view = 'mini',
+--       },
+--     },
+--   },
+-- }
