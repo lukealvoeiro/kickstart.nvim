e57302270db9a01a8a79465da5513d6c70f22601
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-08-22 01:05:31 -0700

    no more stutter on gitstatus load

--- lua/config/custom.lua
+++ lua/config/custom.lua
@@ -1,8 +1,9 @@
 local M = {}
+
 function M.open_git_status(dir)
   vim.fn.chdir(dir)
   vim.cmd 'Git'
-  local timer = vim.loop.new_timer()
+
   local fug_win = vim.api.nvim_get_current_win()
   local fug_buf = vim.api.nvim_win_get_buf(fug_win)
   local ui = vim.api.nvim_list_uis()[1]
@@ -19,32 +20,35 @@ function M.open_git_status(dir)
     style = 'minimal',
     width = win_width,
   }
-  local function mod_fugitive()
-    vim.keymap.set('n', 'q', function()
+
+  -- Immediately set the window configuration
+  vim.api.nvim_win_set_config(fug_win, win_opts)
+
+  -- Set up keymaps and autocmds
+  vim.keymap.set('n', 'q', function()
+    vim.api.nvim_win_close(fug_win, true)
+  end, {
+    buffer = fug_buf,
+    desc = 'Close fugitive window with just q',
+  })
+
+  vim.keymap.set('n', '<Esc>', function()
+    vim.api.nvim_win_close(fug_win, true)
+  end, {
+    buffer = fug_buf,
+    desc = 'Close fugitive window with just <Esc>',
+  })
+
+  local fug_floatwin = vim.api.nvim_create_augroup('fugitiveFloatwin', { clear = true })
+  vim.api.nvim_create_autocmd('BufLeave', {
+    buffer = fug_buf,
+    callback = function()
       vim.api.nvim_win_close(fug_win, true)
-    end, {
-      buffer = fug_buf,
-      desc = 'Close fugitive window with just q',
-    })
-    vim.keymap.set('n', '<Esc>', function()
-      vim.api.nvim_win_close(fug_win, true)
-    end, {
-      buffer = fug_buf,
-      desc = 'Close fugitive window with just <Esc>',
-    })
-    local fug_floatwin = vim.api.nvim_create_augroup('fugitiveFloatwin', { clear = true })
-    vim.api.nvim_create_autocmd('BufLeave', {
-      buffer = fug_buf,
-      callback = function()
-        vim.api.nvim_win_close(fug_win, true)
-        vim.api.nvim_del_augroup_by_id(fug_floatwin)
-      end,
-      desc = 'Close fugitive floating window after we leave it',
-      group = fug_floatwin,
-    })
-    return vim.api.nvim_win_set_config(fug_win, win_opts)
-  end
-  return timer:start(1, 0, vim.schedule_wrap(mod_fugitive))
+      vim.api.nvim_del_augroup_by_id(fug_floatwin)
+    end,
+    desc = 'Close fugitive floating window after we leave it',
+    group = fug_floatwin,
+  })
 end
 
 return M
