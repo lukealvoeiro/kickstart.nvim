c6d41eb98dc47c9e87ad95812dae0c87cda1d542
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-08-25 14:53:09 -0700

    fixes!

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,15 +1,11 @@
 return {
-  'zbirenbaum/copilot-cmp',
-  dependencies = {
-    'zbirenbaum/copilot.lua',
-    config = function()
-      require('copilot').setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      }
-    end,
-  },
+  'zbirenbaum/copilot.lua',
   config = function()
-    require('copilot_cmp').setup()
+    require('copilot').setup {
+      suggestion = { enabled = true, auto_trigger = true, debounce = 50, keymap = {
+        accept = '<C-CR>',
+      } },
+      panel = { enabled = false },
+    }
   end,
 }
