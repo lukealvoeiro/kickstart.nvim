298e0e7b18800f5cdf9608c07b67298dc3e9591b
Luke Alvoeiro <lukejalvoeiro@gmail.com>
2024-09-12 23:45:25 -0700

    wrap up refactor

--- init.lua
+++ init.lua
@@ -11,7 +11,6 @@ require 'core.autocmds'
 -- check out what else is available in noice
 -- fix trouble highlight groups
 -- add trouble as source of diagnostics
--- find a new remap for 's' as i don't use it enough
 -- learn how to use 'g'
 -- trouble diagnostics in lualine
 -- resize buffers/splits using arrow keys
@@ -29,40 +28,6 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
--- [[ Configure and install plugins ]]
---
---  To check the current status of your plugins, run
---    :Lazy
---
---  You can press `?` in this menu for help. Use `:q` to close the window
---
---  To update plugins you can run
---    :Lazy update
-
--- NOTE: Here is where you install your plugins.
-require('lazy').setup({
+require('lazy').setup {
   { import = 'plugins' },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
-  },
-})
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+}
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -32,4 +32,12 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     opts = { signs = false },
   },
+
+  {
+    -- High-performance color highlighter
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end,
+  },
 }
